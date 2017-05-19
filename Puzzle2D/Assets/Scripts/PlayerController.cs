79b1980d8dd4072b6c6d2845dd246ca469b96a2c
OPISTO\roni.sääski <roni.saaski@laajasalonopisto.fi>
2017-05-19 14:31:07 +0300

    PlayerController skripti lisätty, kesken

---
+++ Puzzle2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject debugSprite;
+    Transform sprite;
+    public float debugX;
+    public float debugY;
+    public PlayerGrid grid;
+	// Use this for initialization
+	void Start () {
+        var g = Instantiate(debugSprite);
+        sprite = g.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
