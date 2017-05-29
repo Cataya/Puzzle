3e13faf9500b44d0c5e8e7bc28a913cd2ad65e12
Tuomas Eero <eerotuo@gmail.com>
2017-05-29 13:54:44 +0300

    Forgot I made the previous additions to a test copy of a script

--- Puzzle2D/Assets/Scripts/PlayerController.cs
+++ Puzzle2D/Assets/Scripts/PlayerController.cs
@@ -7,27 +7,60 @@ public class PlayerController : MonoBehaviour {
     Transform sprite;
     public float debugX;
     public float debugY;
+    private float defaultDebugX;
+    private float defaultDebugY;
     public PlayerGrid grid;
     public float velocity;
-    // Use this for initialization
-	void Start () {
-        var g = Instantiate(debugSprite);
+
+    //the falling test sprite
+    GameObject g;
+
+    void Start() {
+        defaultDebugX = debugX;
+        defaultDebugY = debugY;
+
+        g = Instantiate(debugSprite);
         sprite = g.transform;
-	}
-	
-	void Update () {
+    }
+
+    void Update() {
+        //spawnaaminen
+        if (g == null) {
+            g = Instantiate(debugSprite);
+            sprite = g.transform;
+        }
+
         // liikuttaminen
 
         // kääntäminen
 
         // tippuminen (2)
-        debugY = debugY - velocity * Time.deltaTime;
+        if (IsTherePuyoBelow() && g != null) {
+            GameObject d = Instantiate(g);
+            Destroy(g);
+            grid.AddPuyo((int)debugX, (int)debugY + 1, PuyoType.Puyo1, d);
+            debugX = defaultDebugX;
+            debugY = defaultDebugY;
+        }
+        if (!IsTherePuyoBelow()) {
+            debugY = debugY - velocity * Time.deltaTime;                    //Ohjataan spriten liikettä y-akselilla
+        }
 
-        
         // piirtäminen (1)
         float worldX = -(grid.nX - 1) / 2f * grid.gridDistance + debugX * grid.gridDistance;
         float worldY = -(grid.nY - 1) / 2f * grid.gridDistance + debugY * grid.gridDistance;
 
-        sprite.transform.position = new Vector3(worldX, worldY) + grid.transform.position;
+        if (g != null) {
+            sprite.transform.position = new Vector3(worldX, worldY) + grid.transform.position;          //Liikutetaan spritea, riippuvainen debugY:n arvosta
+        }
+    }
+
+    bool IsTherePuyoBelow() {
+        if (grid.grid[(int)debugX][(int)debugY] != PuyoType.None) {
+            print("There is indeed a muthafucking Puyo below!");
+            return true;
+        } else {
+            return false;
+        }
     }
 }
