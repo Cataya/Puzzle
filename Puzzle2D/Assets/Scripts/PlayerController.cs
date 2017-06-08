9226c2890a845208e974175dc769a2ff26786178
Katja-Tietokone\Katja <katja.makela@gmail.com>
2017-06-08 10:46:11 +0300

    2 Puyos is workin now

--- Puzzle2D/Assets/Scripts/PlayerController.cs
+++ Puzzle2D/Assets/Scripts/PlayerController.cs
@@ -44,48 +44,56 @@ public class PlayerController : MonoBehaviour {
         // liikuttaminen
 
         // Puyo1 siirto oikealle ja vasemmalle
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && spawnX1 > 0 && !IsThereObstacleLeft1())
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && spawnX1 > 0 && !IsThereObstacleLeft1() && spawnX2 > 0 && !IsThereObstacleLeft2())
         {
-            spawnX1 = spawnX1 - 1;
-            audioScript.moveSource.Play();
+			audioScript.moveSource.Play();
+			spawnX1 = spawnX1 - 1;
+			spawnX2 = spawnX2 - 1;
+
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && spawnX1 < grid.nX - 1 && !IsThereObstacleRight1())
+		if (Input.GetKeyDown(KeyCode.RightArrow) && spawnX1 < grid.nX - 1 && !IsThereObstacleRight1() && spawnX2 < grid.nX - 1 && !IsThereObstacleRight2())
         {
             audioScript.moveSource.Play();
             spawnX1 = spawnX1 + 1;
+			spawnX2 = spawnX2 + 1;
         }
         // Puyo2 siirto oikealle ja vasemmalle
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && spawnX2 > 0 && !IsThereObstacleLeft2()) {
-            spawnX2 = spawnX2 - 1;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && spawnX2 < grid.nX - 1 && !IsThereObstacleRight2()) {
-            spawnX2 = spawnX2 + 1;
-        }
+//        if (Input.GetKeyDown(KeyCode.LeftArrow) && spawnX2 > 0 && !IsThereObstacleLeft2()) {
+//            spawnX2 = spawnX2 - 1;
+//        }
+//        if (Input.GetKeyDown(KeyCode.RightArrow) && spawnX2 < grid.nX - 1 && !IsThereObstacleRight2()) {
+//            spawnX2 = spawnX2 + 1;
+//        }
         // kääntäminen
 
         // tippuminen (2)
 
         //Tarkistus onko alhaalla jotain edessä?
-        if (IsThereObstacleBelow1() && g1 != null) {
+		if (IsThereObstacleBelow1() && g1 != null || IsThereObstacleBelow2() && g2 != null) {
             grid.AddPuyo(Mathf.FloorToInt(spawnX1), Mathf.FloorToInt(spawnY1 + 1), PuyoType.Puyo2, g1);
-            g1 = null;
+			grid.AddPuyo(Mathf.FloorToInt(spawnX2), Mathf.FloorToInt(spawnY2 + 1), PuyoType.Puyo1, g2);
+			g1 = null;
+			g2 = null;
             spawnX1 = defaultSpawnX1;
             spawnY1 = defaultSpawnY1;
+			spawnX2 = defaultSpawnX2;
+			spawnY2 = defaultSpawnY2;
             StartCoroutine(grid.DropMatchRemove());
         }
-        if (IsThereObstacleBelow2() && g2 != null) {
-            grid.AddPuyo(Mathf.FloorToInt(spawnX2), Mathf.FloorToInt(spawnY2 + 1), PuyoType.Puyo1, g2);
-            g2 = null;
-            spawnX2 = defaultSpawnX2;
-            spawnY2 = defaultSpawnY2;
-            StartCoroutine(grid.DropMatchRemove());
-        }
-        if (!IsThereObstacleBelow1()/* && IsThereObstacleLeft() && IsThereObstacleRigh()*/) {
-            spawnY1 = spawnY1 - velocity * Time.deltaTime;                    //Ohjataan spriten liikettä y-akselilla
-        }
-        if (!IsThereObstacleBelow2()/* && IsThereObstacleLeft() && IsThereObstacleRigh()*/) {
-            spawnY2 = spawnY2 - velocity * Time.deltaTime;                    //Ohjataan spriten liikettä y-akselilla
-        }
+//        if (IsThereObstacleBelow2() && g2 != null) {
+//            grid.AddPuyo(Mathf.FloorToInt(spawnX2), Mathf.FloorToInt(spawnY2 + 1), PuyoType.Puyo1, g2);
+//            g2 = null;
+//            spawnX2 = defaultSpawnX2;
+//            spawnY2 = defaultSpawnY2;
+//            StartCoroutine(grid.DropMatchRemove());
+//        }
+		if (!IsThereObstacleBelow1() || !IsThereObstacleBelow2()) {
+            spawnY1 = spawnY1 - velocity * Time.deltaTime;
+			spawnY2 = spawnY2 - velocity * Time.deltaTime; //Ohjataan spriten liikettä y-akselilla
+//        }
+//        if (!IsThereObstacleBelow2()/* && IsThereObstacleLeft() && IsThereObstacleRigh()*/) {
+//            spawnY2 = spawnY2 - velocity * Time.deltaTime;                    //Ohjataan spriten liikettä y-akselilla
+//        }
 
         // piirtäminen (1)
         float worldX1 = -(grid.nX - 1) / 2f * grid.gridDistance + spawnX1 * grid.gridDistance;
@@ -102,7 +110,7 @@ public class PlayerController : MonoBehaviour {
             sprite2.transform.position = new Vector3(worldX2 + grid.gridDistance, worldY2) + grid.transform.position;
         }
     }
-
+	}
     bool IsThereObstacleBelow1() {
         return grid.grid[(int)spawnX1][(int)spawnY1] != PuyoType.None || spawnY1 < 0;
     }
