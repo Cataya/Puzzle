972547efcdad214f8975c617ec5c1782af490b68
Yrjö Peussa <peussa@iki.fi>
2017-06-09 11:10:22 +0300

    Puyo generator v1, untested

---
+++ Puzzle2D/Assets/Scripts/PuyoGenerator.cs
@@ -0,0 +1,50 @@
+﻿// Ykän mestariteos, 25 min. Ei testattu :-p
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuyoGenerator : MonoBehaviour {
+    public int minimumInQueue = 5;
+
+    List<List<PuyoType>> p1puyos;
+    List<List<PuyoType>> p2puyos;
+
+    static PuyoType[] generatorPool = { PuyoType.Puyo1, PuyoType.Puyo2, PuyoType.Puyo3, PuyoType.Puyo4 };
+
+    void Awake() {
+        InitAtLevelStart();   
+    }
+
+    public void InitAtLevelStart() {
+        p1puyos = new List<List<PuyoType>>();
+        p2puyos = new List<List<PuyoType>>();
+        GenerateEnoughNewPuyos();
+    }
+
+    public List<PuyoType> GetNextPuyos(int player) {
+        var thisQ = player == 1 ? p1puyos : p2puyos;
+        var temp = thisQ[0];
+        thisQ.RemoveAt(0);
+        GenerateEnoughNewPuyos();
+        return temp;
+    }
+
+    // true = player 1, false = player 2
+    public List<PuyoType> PeekNextPuyos(int player, int index) {
+        return (player == 1 ? p1puyos : p2puyos)[index];
+    }
+
+    void GenerateEnoughNewPuyos() {
+        while ( p1puyos.Count < minimumInQueue ||
+                p2puyos.Count < minimumInQueue) {
+            var newPuyoSet = new List<PuyoType>() {
+                generatorPool[Random.Range(0, generatorPool.Length)],
+                generatorPool[Random.Range(0, generatorPool.Length)]
+            };
+            var copy = new List<PuyoType>(newPuyoSet);
+            p1puyos.Add(newPuyoSet);
+            p2puyos.Add(copy);
+        }
+    }
+}
