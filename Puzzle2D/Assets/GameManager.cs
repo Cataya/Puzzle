b400a7fdcfe6df764e7cbe84a8f93952fe099dba
OPISTO\katja.mäkelä <katja.makela@gmail.com>
2017-06-12 12:22:21 +0300

    GameOver? Not working.

--- Puzzle2D/Assets/GameManager.cs
+++ Puzzle2D/Assets/GameManager.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public PlayerController pc1, pc2;
+    public PlayerGrid grid1, grid2;
     public void GameOver(int playerId) {
-
+        print("Game Over\n Winner is player " + playerId);
+        pc1.enabled = false;
+        pc2.enabled = false;
+        enabled = false;
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void update() {
+ //   public void IsGameOver() { 
+        if (grid1.grid[(int)pc1.defaultSpawnX1][(int)pc1.defaultSpawnY1] != PuyoType.None) {
+            GameOver(2);
+            print("loppuu");
+        }
+        if (grid2.grid[(int)pc2.defaultSpawnX2][(int)pc2.defaultSpawnY2] != PuyoType.None) {
+            GameOver(1);
+            print("Loppuu");
+        }
+        else {
+            print("Peli jatkuu");
+        }
+    }
 }
