1540d9f0f8f2eeea7837306da4d3db464065d400
OPISTO\katja.mäkelä <katja.makela@gmail.com>
2017-06-12 13:31:41 +0300

    GameOver

--- Puzzle2D/Assets/GameManager.cs
+++ Puzzle2D/Assets/GameManager.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public PlayerController pc1, pc2;
-    public PlayerGrid grid1, grid2;
+    int winner;
     public void GameOver(int playerId) {
-        print("Game Over\n Winner is player " + playerId);
+        if (playerId == 1) {
+            winner = 2;
+        }
+        if (playerId == 2) {
+            winner = 1;
+        }
+        print("Game Over\n Winner is player " + winner );
+
         pc1.enabled = false;
         pc2.enabled = false;
         enabled = false;
-    }
-
-    // Update is called once per frame
-    void update() {
- //   public void IsGameOver() { 
-        if (grid1.grid[(int)pc1.defaultSpawnX1][(int)pc1.defaultSpawnY1] != PuyoType.None) {
-            GameOver(2);
-            print("loppuu");
-        }
-        if (grid2.grid[(int)pc2.defaultSpawnX2][(int)pc2.defaultSpawnY2] != PuyoType.None) {
-            GameOver(1);
-            print("Loppuu");
-        }
-        else {
-            print("Peli jatkuu");
-        }
     }
 }
