18a2073efd14da6ca11b78d7e8ec0e82f084cbd9
OPISTO\katja.mäkelä <katja.makela@gmail.com>
2017-06-13 10:39:35 +0300

    Main and Menu scenes

---
+++ Puzzle2D/Assets/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void LoadGame() {
+            SceneManager.LoadScene("Main"); //Ladataan kenttä, jonka nimi on muuttujassa
+        }
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
+
